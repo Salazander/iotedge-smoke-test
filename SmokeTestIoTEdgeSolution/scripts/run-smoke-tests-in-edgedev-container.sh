8f129eb52385b7fdb5e032b53d86a7478396dd88
Alexander Gassmann <agassman@microsoft.com>
2021-11-08 15:40:52 +0100

    Initial commit introducing smoke testing
    * iot edge solution
    * devcontainer for VSCode
    * bash scripts running the smoke tests

---
+++ SmokeTestIoTEdgeSolution/scripts/run-smoke-tests-in-edgedev-container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euxo pipefail
+#!cp ../config/deployment.debug.amd64.json ./deployment.json
+source .env
+
+sudo docker run \
+    --rm \
+    --name IoTEdgeDevContainer \
+    --entrypoint /bin/bash \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $(pwd):/home/iotedge \
+    --env DEVICE_CONNECTION_STRING="$DEVICE_CONNECTION_STRING" \
+    mcr.microsoft.com/iotedge/iotedgedev:3.1.0-amd64 \
+    -c "./scripts/run-smoke-tests.sh $SMOKE_TESTS_RUNTIME_IN_SECONDS $SMOKE_TESTS_LOG_WINDOW_IN_SECONDS '$SMOKE_SIGNS_TO_GREP'"
